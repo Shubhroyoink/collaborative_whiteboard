d60aff729d2d2885badd6deba5c159d620a4250d
Shubhroyoink <24cs3051@rgipt.ac.in>
2025-12-31 22:29:42 +0530

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>🧠 Real-Time Collaborative Whiteboard</h1>
+      <p>
+        Welcome! This project will allow multiple users to draw together in real time.
+      </p>
+      <button>Start Drawing</button>
+    </div>
+  )
+}
+
+export default App
